b649b7e9b164d73eba6800b546e5a5b4fb704cfe
mdwoicke <55119044+mdwoicke@users.noreply.github.com>
2024-07-07 23:28:26 -0500

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import type { AppInfo } from '@/types/app'
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `http://dify-awuac-u15194.vm.elestio.app/v1`
-export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
+export const IS_WORKFLOW = `false` 
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
   description: 'App description',
