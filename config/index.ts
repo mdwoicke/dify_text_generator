61bf3ed5fc539fa5c80c9cdb1901e57eb86c1d4e
mdwoicke <55119044+mdwoicke@users.noreply.github.com>
2024-07-08 07:07:29 -0500

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import type { AppInfo } from '@/types/app'
 export const APP_ID = '3cd8cb4b-3db4-4a0c-bcfb-a68a8aca3326' 
 export const API_KEY = 'app-GAkz33MTKX8DWk1OI8PlbVBF'
 export const API_URL = 'https://dify-awuac-u15194.vm.elestio.app/v1'
-export const IS_WORKFLOW = 'false'
+export const IS_WORKFLOW = 'true'
 export const APP_INFO: AppInfo = {
   title: 'Generator APP',
   description: 'App description',
