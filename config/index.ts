f326de488962494d2734419b4760acb456548e02
Mike Woicke <mwoicke@doublepositive.com>
2024-07-08 19:26:32 -0500

    Updated

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,8 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = '3cd8cb4b-3db4-4a0c-bcfb-a68a8aca3326' 
-export const API_KEY = 'app-GAkz33MTKX8DWk1OI8PlbVBF'
-export const API_URL = 'https://dify-awuac-u15194.vm.elestio.app/v1'
-export const IS_WORKFLOW = 'true'
+export const APP_ID = '1049d466-df04-42b9-a87c-71867850ca79'
+export const API_KEY = 'app-2a1Xx4aH4nL4o4luWVqNYYkz'
+export const API_URL = 'https://api.dify.ai/v1'
+export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
   title: 'Generator APP2',
   description: 'App description',
@@ -12,7 +12,7 @@ export const APP_INFO: AppInfo = {
 }
 
 
-export const API_PREFIX = '/app'
+export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
